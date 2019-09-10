5158848a177429bc63e7a329bf2a75d4e047cf42
Roc-zhou <13031902163@163.com>
2019-09-10 09:31:46 +0800

    add

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import wxShare from './wxconfig'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$wxShare = wxShare
 
 /* eslint-disable no-new */
 new Vue({
